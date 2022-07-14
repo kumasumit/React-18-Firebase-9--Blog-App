827af3ad55061a318ac125417e0957d016aac962
Kumar Sumit <kumarsumit1989@gmail.com>
2022-07-14 18:47:34 +0530

    Create Home and Post Detail Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import {collection,getDocs, doc } from "firebase/firestore";
+import {db} from "../firebase";
+
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    
+async function getPosts (coll) {
+  const newPosts=[];
+  const querySnapshot = await getDocs(collection(db, coll));
+ 
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const post = doc.data();
+      post.id = doc.id;
+      newPosts.push(post);
+    });
+    console.log('posts', newPosts);
+    setPosts(newPosts);
+}
+
+getPosts("posts");    
+}, [])
+  
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Kumar Sumit</div>
+      {/* Map through the posts array */}
+      {posts.map((post, index)=>(
+        <div className='post' key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subTitle}</p>
+        </div>
+      ))
+
+      }
+    </div>
   )
 }
 
--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import { getDoc, doc } from "firebase/firestore";
+import {db} from "../firebase";
+
 
 const PostDetail = () => {
+  const [post, setPost] = useState([]);
+  const {postId} = useParams();
+  useEffect(() => {
+    async function getDocument (coll, id) {
+      const snap = await getDoc(doc(db, coll, id))
+      if (snap.exists()){
+        console.log('snapshot', snap.data());
+        setPost(snap.data());
+      }
+        
+      else
+        return Promise.reject(Error(`No such document: ${coll}.${id}`))
+    }
+    
+    getDocument("posts", postId);
+    
+  }, [])
+  
   return (
-    <div>PostDetail</div>
+    <div className='post-detail'>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
   )
 }
 
